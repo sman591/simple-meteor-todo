9d512751ed84866a7a34f32b780c0ca464f89b34
Stuart Olivera <stuart@stuartolivera.com>
2014-12-22 01:57:15 -0500

    Completed update and remove tutorial

--- simple-todo.js
+++ simple-todo.js
@@ -4,11 +4,11 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-  tasks: function () {
-    // Show newest tasks first
-    return Tasks.find({}, {sort: {createdAt: -1}});
-  }
-});
+    tasks: function () {
+      // Show newest tasks first
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
 
   Template.body.events({
     "submit .new-task": function (event) {
@@ -28,4 +28,14 @@ if (Meteor.isClient) {
       return false;
     }
   });
+
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  })
+
 }
